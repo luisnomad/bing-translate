1c7a3fd53fc71cfdadd193d26a152a45c04b30b0
Alexandru Marian Vasile <alexandru.vasile@cmbtravel.ro>
2014-01-16 15:12:51 +0200

    initial commit

--- demo.js
+++ demo.js
@@ -1,17 +1,3 @@
-bing-translate
-==============
-
-Bing Translator module for node.js
-
-## Installation
-
-```js
-$ npm install bing-translate
-```
-
-## API
-
-```js
 var bt = require('./lib/bing-translate.js').init({
     client_id: 'your_client_id', 
     client_secret: 'your_client_secret'
@@ -20,4 +6,3 @@ var bt = require('./lib/bing-translate.js').init({
 bt.translate('This hotel is located close to the centre of Paris.', 'en', 'ro', function(err, res){
   console.log(err, res);
 });
-```
